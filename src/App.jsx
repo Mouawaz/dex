bdafb14d7db152d7f0274e3ca07e936d007c26a8
Luca <Lucamouawad1@gmail.com>
2025-04-27 12:18:01 +0200

    addition to githut pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 import PokemonDetail from './components/PokemonDetail';
 import './App.css';
